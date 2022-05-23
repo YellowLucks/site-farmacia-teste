0e254ec10b240573f5cc0ca45365e431c52cd95d
Lucas Aleixo <lucasaleixo18@gmail.com>
2022-05-22 21:13:19 -0300

    novo fix

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 import flask
+import os
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
+
 
 @app.route("/contatos")
 def contatos():
     return render_template("contatos.html")
 
+
 @app.route("/usuarios/<nome_usuario>")
 def usuarios(nome_usuario):
     return render_template("usuario.html", nome_usuario=nome_usuario)
 
+
 if __name__=="__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
